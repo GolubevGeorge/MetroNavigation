c4297dc1e92f94e3cca9efa5338cfca10d4e9228
George <golubev_georgiy@ukr.net>
2017-05-08 23:01:55 +0300

    Second commit

--- MetroNavigation/MainWindow.xaml.cs
+++ MetroNavigation/MainWindow.xaml.cs
@@ -1,28 +1,91 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MetroNavigation
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow
     {
+        LocationCollection locations = new MetroLines().AddStations();
+        LocationCollection lc = new LocationCollection();
+        int startIndex = 0;
+        int endIndex = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            addImageToMap();
+        }
+
+        public void addImageToMap()
+        {
+            foreach (var location in locations)
+            {
+                Image image = new Image();
+                image.Source = new BitmapImage(new Uri(@"/Resources/Metro.png", UriKind.Relative));
+                image.Width = 30;
+                image.Height = 30;
+                image.Opacity = 0.6;
+
+                MapLayer.SetPosition(image, location);
+                MapLayer.SetPositionOrigin(image, PositionOrigin.BottomCenter);
+                MainMap.Children.Add(image);
+
+                image.MouseLeftButtonDown += Image_LeftButtonDown;
+                image.MouseRightButtonDown += Image_MouseRightButtonDown;
+            }
+
+            MapPolyline polyLine = new MapPolyline();
+            polyLine.Stroke = new SolidColorBrush(Colors.Red);
+            polyLine.StrokeThickness = 6;
+            polyLine.Opacity = 0.4;
+            polyLine.Locations = locations;
+            MainMap.Children.Add(polyLine);
+        }
+
+        private void Image_LeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            startIndex = locations.IndexOf(MapLayer.GetPosition(((Image)sender)));
+        }
+
+        private void Image_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            endIndex = locations.IndexOf(MapLayer.GetPosition(((Image)sender)));
+            Calculate();
+        }
+
+        public void Calculate()
+        {
+            lc.Clear();
+
+            foreach (var item in locations)
+            {
+                if (locations.IndexOf(item) >= startIndex & locations.IndexOf(item) <= endIndex)
+                {
+                    lc.Add(item);
+                }
+                else if (locations.IndexOf(item) >= endIndex & locations.IndexOf(item) <= startIndex)
+                {
+                    lc.Add(item);
+                }
+            }
+
+            MapPolyline path = new MapPolyline();
+            path.Stroke = new SolidColorBrush(Colors.Green);
+            path.StrokeThickness = 10;
+            path.Opacity = 1;
+            path.Locations = lc;
+            MainMap.Children.Remove(path);
+            MainMap.Children.Add(path);
         }
     }
 }
+
---
+++ MetroNavigation/MetroLines.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroNavigation
+{
+    class MetroLines
+    {
+        LocationCollection redLine = new LocationCollection();
+
+        public LocationCollection  AddStations()
+        {
+            
+            redLine.Add(new Location(50.464936, 30.355272000000014));
+            redLine.Add(new Location(50.4560768, 30.364998199999945));
+            redLine.Add(new Location(50.457771, 30.390584999999987));
+            redLine.Add(new Location(50.458631, 30.403914999999984));
+            redLine.Add(new Location(50.45927, 30.41901400000006));
+            redLine.Add(new Location(50.454489, 30.447238999999968));
+            redLine.Add(new Location(50.450953, 30.46514400000001));
+            redLine.Add(new Location(50.44139, 30.489041000000043));
+            redLine.Add(new Location(50.442925, 30.503717999999935));
+            redLine.Add(new Location(50.444851, 30.516073000000006));
+            redLine.Add(new Location(50.447056, 30.525360999999975));
+            redLine.Add(new Location(50.443017, 30.547732999999994));
+            redLine.Add(new Location(50.442203, 30.557758000000035));
+            redLine.Add(new Location(50.445937, 30.57689400000004));
+            redLine.Add(new Location(50.451877, 30.59816699999999));
+            redLine.Add(new Location(50.45574509999999, 30.611898500000052));
+            redLine.Add(new Location(50.45992099999999, 30.630374999999958));
+            redLine.Add(new Location(50.464778, 30.645609000000036));
+
+            return redLine;
+        }
+    }
+}
